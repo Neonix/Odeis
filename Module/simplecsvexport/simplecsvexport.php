5ebf3d1a322c69daef1e1b398884aba275078233
Neonix <Neonix@lovewow.fr>
2017-12-17 23:47:53 +0100

    Fix mail ...

--- Module/simplecsvexport/simplecsvexport.php
+++ Module/simplecsvexport/simplecsvexport.php
@@ -13,7 +13,7 @@
  * obtain it through the world-wide-web, please send an email
  * to ecommerce@quadra-informatique.fr so we can send you a copy immediately.
  *
- * @author    Nils NICOLAS <Neonix@lovewow.fr>
+ * @author    Nils NICOLAS <nils@oneshot.nc>
  * @copyright 2018 Oneshot
  * @version Release: $Revision: 1.0.0 $
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
